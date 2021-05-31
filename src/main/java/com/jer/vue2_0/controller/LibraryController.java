b4f1379859d077d9e7ddd58dc68568faced6b5e9
wubowen2018 <wubowen2017@gmail.com>
2021-05-31 18:00:16 +0800

    添加上传图片

--- src/main/java/com/jer/vue2_0/controller/LibraryController.java
+++ src/main/java/com/jer/vue2_0/controller/LibraryController.java
@@ -2,9 +2,13 @@ package com.jer.vue2_0.controller;
 
 import com.jer.vue2_0.pojo.Book;
 import com.jer.vue2_0.service.BookService;
+import com.jer.vue2_0.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -51,6 +55,26 @@ public class LibraryController {
             return bookService.Search(keywords);
         }
     }
+
+    @CrossOrigin
+    @PostMapping("api/covers")
+    public String coversUpload(MultipartFile file) throws Exception {
+        String folder = "D:/workspace/img";
+        File imageFolder = new File(folder);
+        File f = new File(imageFolder, StringUtils.getRandomString(6) + file.getOriginalFilename()
+                .substring(file.getOriginalFilename().length() - 4));
+        if (!f.getParentFile().exists())
+            f.getParentFile().mkdirs();
+        try {
+            file.transferTo(f);
+            String imgURL = "http://localhost:8443/api/file/" + f.getName();
+            return imgURL;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
 
 
 
