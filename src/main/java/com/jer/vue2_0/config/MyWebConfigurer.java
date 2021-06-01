caf71e3beda469b39e583079810265980f84305e
wubowen2018 <wubowen2017@gmail.com>
2021-06-01 21:06:50 +0800

    配置 webConfig

---
+++ src/main/java/com/jer/vue2_0/config/MyWebConfigurer.java
@@ -0,0 +1,27 @@
+package com.jer.vue2_0.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MyWebConfigurer extends WebMvcConfigurationSupport {
+
+    //实现跨域
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //所有请求都允许跨域
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+    //把URL和图片资源文件夹 d:/workspace/img/ 对应起来
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/file/**").addResourceLocations("file:" + "d:/workspace/img/");
+        super.addResourceHandlers(registry);
+    }
+
+}
