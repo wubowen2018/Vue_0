c57e7388507f2f261cb853e77afeaed27de8f389
wubowen2018 <wubowen2017@gmail.com>
2021-07-05 14:20:12 +0800

    日志格式 存档设置 请求和响应打印 分出开发环境配置文件

--- src/main/java/com/jer/vue2_0/controller/LoginController.java
+++ src/main/java/com/jer/vue2_0/controller/LoginController.java
@@ -1,17 +1,19 @@
 package com.jer.vue2_0.controller;
 
+import com.jer.vue2_0.config.annotation.LogPrint;
 import com.jer.vue2_0.pojo.User;
 import com.jer.vue2_0.result.Result;
 import com.jer.vue2_0.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
 
-
 @Controller
+@Slf4j
 public class LoginController {
 
     @Autowired
@@ -20,6 +22,7 @@ public class LoginController {
     @CrossOrigin
     @PostMapping(value = "api/login")
     @ResponseBody
+    @LogPrint(description ="用户登录")
     public Result login( @RequestBody User requestUser){
         String username = requestUser.getUsername();
         username = HtmlUtils.htmlEscape(username);
